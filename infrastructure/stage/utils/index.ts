bcefd9c19954e76020551a9c6433b4179b3fb25f
Alexis Lucattini <alexis.lucattini@umccr.org>
2026-01-30 20:56:20 +1100

    Deleted the wrong version of the camel case function (#31)

--- infrastructure/stage/utils/index.ts
+++ infrastructure/stage/utils/index.ts
@@ -1,5 +1,5 @@
-export function camelCaseToKebabCase(camelCase: string): string {
-  return camelCase.replace(/([A-Z])/g, '-$1').toLowerCase();
+export function camelCaseToKebabCase(input: string): string {
+  return input.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
 }
 
 export function camelCaseToSnakeCase(input: string): string {
