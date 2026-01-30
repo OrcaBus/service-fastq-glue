20e54d1577dc939378befc0ab7a7222d70e8ab1f
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2026-01-30 20:40:48 +1100

    Camelcase to kebabcase function duplication fix

--- infrastructure/stage/utils/index.ts
+++ infrastructure/stage/utils/index.ts
@@ -5,6 +5,3 @@ export function camelCaseToKebabCase(camelCase: string): string {
 export function camelCaseToSnakeCase(input: string): string {
   return input.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase();
 }
-export function camelCaseToKebabCase(input: string): string {
-  return input.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
-}
