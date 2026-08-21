13b602325aa7d0f46cea718e78383dd7a1937b84
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2026-08-21 10:40:18 +1000

    feat: migrate to platform-cdk-constructs 1.7.2 and use GitStack
    - Upgrade @orcabus/platform-cdk-constructs from 1.5.4 to 1.7.2
    - Update StatelessApplicationStack to extend GitStack
    - Update StatefulApplicationStack to extend GitStack
    - Both stacks now emit a GitCommitId CloudFormation output for
      deployment traceability

--- infrastructure/stage/stateless-application-stack.ts
+++ infrastructure/stage/stateless-application-stack.ts
@@ -10,10 +10,11 @@ import { buildAllStepFunctions } from './step-functions';
 import { buildAllEventRules } from './event-rules';
 import { NagSuppressions } from 'cdk-nag';
 import { StageName } from '@orcabus/platform-cdk-constructs/shared-config/accounts';
+import { GitStack } from '@orcabus/platform-cdk-constructs/deployment-stack-pipeline';
 
 export type StatelessApplicationStackProps = cdk.StackProps & StatelessApplicationStackConfig;
 
-export class StatelessApplicationStack extends cdk.Stack {
+export class StatelessApplicationStack extends GitStack {
   public readonly stageName: StageName;
   constructor(scope: Construct, id: string, props: StatelessApplicationStackProps) {
     super(scope, id, props);
